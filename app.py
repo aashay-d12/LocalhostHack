0b72c6b35b649595b393b114a5a691242a541f5b
zwwix <161281880+zwwix@users.noreply.github.com>
2024-03-02 21:26:30 +0000

    comit

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ Session(app)
 
 db = SQL("sqlite:///healthquest.db")
 
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -21,6 +20,13 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+@app.context_processor
+def inject_user():
+    if session.get("user_id"):
+        user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
+        if user:
+            return dict(user=user[0])
+    return dict(user=None)
 
 def login_required(f):
     @wraps(f)
@@ -31,9 +37,11 @@ def login_required(f):
 
     return decorated_function
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    session.clear()
     if request.method == "POST":
         if not request.form.get("username"):
             return render_template("login.html", info="username invalid")
@@ -50,3 +58,49 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        passw = request.form.get("password")
+        rpassw = request.form.get("rpassword")
+        if (
+            not username
+            or len(db.execute("SELECT * FROM users WHERE username = ?", username)) != 0
+        ):
+            return render_template("register.html", info="username taken/invalid")
+        if passw != rpassw or not passw:
+            return render_template(
+                "register.html", info="password invalid / password do not match"
+            )
+        db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)",
+            username,
+            generate_password_hash(passw),
+        )
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", username
+        )
+        session["user_id"] = rows[0]["id"]
+    return redirect("/")
+
+@app.route("/emergency")
+def emergency():
+    return render_template("emergency.html")
+
+@app.route("/quickcheck")
+def progress():
+        return render_template("progress.html")
+
+@app.route("/challenges")
+def challenges():
+        return render_template("challenges.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
